b171b0c9fd2f7e7002d8d67bba3499e1c088757b
Luna Simons <luna@bddvlpr.com>
2025-07-23 20:03:16 +0200

    chore: initialize monorepo structure

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+import antfu from '@antfu/eslint-config';
+
+export default antfu({
+  stylistic: {
+    semi: true,
+  },
+  typescript: true,
+  svelte: true,
+  ignores: [
+    '**/src-tauri/gen',
+  ],
+});
